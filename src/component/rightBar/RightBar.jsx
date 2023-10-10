3fd7d75c63ac88e85f642029faf34b85dcf8c0b2
nodeSkin <shittujamiu@gmail.com>
2023-10-10 23:50:06 +0100

    updates on UI

--- src/component/rightBar/RightBar.jsx
+++ src/component/rightBar/RightBar.jsx
@@ -3,7 +3,22 @@ import "./rightBar.css"
 export default function RightBar() {
   return (
     <div className="rightBar">
-      RightBar
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="assets/icon2.png
+          " alt="" />
+          <span className="birthdayText"><b>Akinboyewa Dare</b> and <b>3 other friends</b> have a birthday today  </span>
+        </div>
+        <img className="rigtBarAd" src="assets/post/p5.jpg" alt="" />
+        <h4 className="rightBarTitle">Online Friends</h4>
+        <ul className="rightBarFriendList">
+          <li className="rightBarFriend">
+            <div className="rightBarProfileImgContainer">
+              <img className="rightBarProfileImg" src="" alt=""  />
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
